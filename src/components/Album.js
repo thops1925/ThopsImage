41ba5b19d07874c2d264f34568b57eb9e7603afa
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-09-22 03:36:37 +0800

    fix slide index

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,27 +2,28 @@ import React, { useState } from 'react';
 import Slider from './Slider';
 
 export default function Album({ photo }) {
-  const [isDataSlide, setDataSlide] = useState([]);
-  const modal = (photo) => {
-    setDataSlide(photo);
+  const [isDataSlide, setDataSlide] = useState();
+  const modal = (index) => {
+    setDataSlide(index);
   };
   return (
     <>
       <div className="grid md:grid-cols-3 items-center justify-center md:px-16 ">
-        {photo.map((item) => {
+        {photo.map((item, index) => {
           return (
             <div key={item.id}>
               <img
+                key={index}
                 className="md:object-cover object-cover h-96 w-full md:p-2"
                 src={item}
                 alt=""
-                onClick={() => modal(photo)}
+                onClick={() => modal(index)}
               />
             </div>
           );
         })}
       </div>
-      <Slider dataSlider={isDataSlide} />
+      <Slider dataSlider={isDataSlide} data={photo} />
     </>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,35 +1,35 @@
-import React from 'react';
-// import {
-//   FaArrowCircleLeft,
-//   FaArrowCircleRight,
-//   FaRegWindowClose,
-// } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import {
+  FaArrowCircleLeft,
+  FaArrowCircleRight,
+  FaRegWindowClose,
+} from 'react-icons/fa';
 
-export default function Slider({ dataSlider }) {
-  //   const [islenght, setlength] = useState();
-  // const [isSlide, setSlide] = useState();
+export default function Slider({ dataSlider, data }) {
+  const [isSlider, setSlider] = useState('');
+  useEffect(() => {
+    setSlider();
+  }, []);
+  console.log(isSlider);
 
-  // console.log(isSlide);
-  //   const [isSlider, setSlider] = useState(0);
-
-  //   const rightArrow = () => {
-  //     setSlider(isSlider === slide.length - 1 ? 0 : isSlider + 1);
-  //   };
-  //   const leftArrow = () => {
-  //     setSlider(isSlider === 0 ? slide.length - 1 : isSlider - 1);
-  //   };
+  const rightArrow = () => {
+    setSlider(dataSlider + 1);
+  };
+  const leftArrow = () => {
+    setSlider(dataSlider - 1);
+  };
 
   return (
     <>
       <div className=" flex justify-center items-center">
-        {/* <FaRegWindowClose />
+        <FaRegWindowClose />
         <FaArrowCircleRight onClick={rightArrow} />
-        <FaArrowCircleLeft onClick={leftArrow} /> */}
-        {dataSlider.map((item) => {
-          return (
-            <img src={item} alt="" className="w-auto h-screen object-contain" />
-          );
-        })}
+        <FaArrowCircleLeft onClick={leftArrow} />
+        <img
+          src={data[dataSlider]}
+          alt=""
+          className="w-auto h-screen object-contain"
+        />
       </div>
     </>
   );
