3c3aa2fb608dc0d7416e2916dca7f52dc2649cba
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-09-24 05:05:09 +0800

    edit home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import Cover from '../Data/Cover';
 import About from '../Data/About';
+import Card from '../components/Card';
 
 export default function Home() {
   const [{ title, description, story }] = About;
   return (
-    <div
-      className="min-h-screen w-screen flex justify-center
-     pt-10 flex-col md:flex-row bg-gray-100 md:px-32 md:py-8"
-    >
-      <div className="md:px-32 md:py-32 font-sans">
-        <p
-          className="flex border-gray-200 text-gray-600 
-          pb-8 border-b-2  text-6xl justify-center font-cursive font-thin"
-        >
-          {title}
-        </p>
+    <>
+      <div
+        className="min-h-screen w-screen flex justify-center 
+    item-center flex-col lg:flex-row lg:justify-around select-none"
+      >
+        <div className=" font-sans my-5 flex justify-center items-center flex-col lg:max-w-xl w-screen">
+          <p
+            className=" border-gray-200 text-gray-600 
+            text-6xl justify-center items-start font-cursive font-thin"
+          >
+            {title}
+          </p>
 
-        <p className=" text-gray-600  justify-center p-10 font-thops ">
-          {story}
-        </p>
-        <p className=" text-gray-600  justify-center px-10 pb-10">
-          {description}
-        </p>
+          <p className=" text-justify  text-gray-600  flex justify-center items-center font-thops ">
+            {story}
+          </p>
+          <p className="text-justify text-gray-600 flex justify-center items-center">
+            {description}
+          </p>
+        </div>
+
+        <img
+          className="lg:max-w-xl w-screen my-5 rounded-sm"
+          src={Cover}
+          alt=""
+        />
+        <Card />
       </div>
-
-      <img className="md:pr-32 rounded " src={Cover} alt="" />
-    </div>
+    </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center  h-16 bg-gray-900 text-white ">
-      <p>Copyright</p>
+    <div className="flex justify-center items-center flex-col  h-64 bg-gray-900 text-gray-400 ">
+      <div className="mb-3">
+        <FacebookRoundedIcon className="mx-2 " />
+        <InstagramIcon className="mx-2" />
+        <GitHubIcon className="mx-2" />
+      </div>
+      <p className="flex text-sm">
+        &copy;2021 ThopsImagery. All right reserved{' '}
+      </p>
     </div>
   );
 }
