7fe0f333e546131ed3043f72a73b7c5336d7b602
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-08-20 17:34:28 +0800

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,8 +14,5 @@ module.exports = {
       },
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
